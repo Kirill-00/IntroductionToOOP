1490d18adb66d3e6def0d3bba47ab185f48781ee
Kirill <levin.kirill03@gmail.com>
2022-11-07 15:21:55 +0300

    String DONE

--- String/Source.cpp
+++ String/Source.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
 using namespace std;
 
+class String;
+String operator+(const String& left, const String& right);
+
 class String
 {
 	size_t size; //размер строки в Байтах
@@ -14,7 +17,7 @@ public:
 	{
 		return str;
 	}
-	int get_size()const
+	size_t get_size()const
 	{
 		return size;
 	}
@@ -39,6 +42,15 @@ public:
 		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
 		cout << "CopyConstructor:" << this << endl;
 	}
+	String(String&& other)
+	{
+		//Shallow copy:
+		this->size = other.size;
+		this->str = other.str;
+		other.size = 0;
+		other.str = nullptr;
+		cout << "MoveConstructor:" << this << endl;
+	}
 	~String()
 	{
 		delete[] this->str;
@@ -55,6 +67,30 @@ public:
 		cout << "CopyAssigment:\t" << this << endl;
 		return *this;
 	}
+	String& operator=(String&& other)
+	{
+		if (this == &other)return *this;
+		delete[] this->str;
+		this->size = other.size;
+		this->str = other.str;
+		other.size = 0;
+		other.str = nullptr;
+		cout << "MoveAssignment:\t" << this << endl;
+		return *this;
+	}
+	String& operator+=(const String& other)
+	{
+		return *this = *this + other;
+	}
+
+	char operator[](int i)const		//i - index
+	{
+		return str[i];
+	}
+	char& operator[](int i)
+	{
+		return str[i];
+	}
 	//                Methods:
 	void print()const
 	{
@@ -62,17 +98,17 @@ public:
 		cout << "str:\t" << str << endl;
 	}
 };
-
 String operator+(const String& left, const String& right)
 {
 	String cat(left.get_size() + right.get_size() - 1);
 	for (int i = 0; i < left.get_size(); i++)
-		cat.get_str()[i] = left.get_str()[i];
+		cat[i] = left[i];
+	//cat.get_str()[i] = left.get_str()[i];
 	for (int i = 0; i < right.get_size(); i++)
-		cat.get_str()[i + left.get_size() - 1] = right.get_str()[i];
+		cat[i + left.get_size() - 1] = right[i];
+	//cat.get_str()[i + left.get_size() - 1] = right.get_str()[i];
 	return cat;
 }
-
 ostream& operator<<(ostream& os, const String& obj)
 {
 	return os << obj.get_str();
@@ -106,7 +142,10 @@ void main()
 #ifdef OPERATOR_PLUS_CHECK
 	String str1 = "Hello";
 	String str2 = "World";
-	String str3 = str1 + " " + str2;
-	cout << str3 << endl;
+	/*String str3;
+	str3 = str1 + str2;
+	cout << str3 << endl;*/
+	str1 += str2;
+	cout << str1 << endl;
 #endif // OPERATOR_PLUS_CHECK
 }
