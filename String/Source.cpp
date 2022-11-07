2111c62cfa4174465f74d2ca9f4376939f44bdff
Kirill <levin.kirill03@gmail.com>
2022-11-07 22:00:02 +0300

    enciding

--- String/Source.cpp
+++ String/Source.cpp
@@ -116,6 +116,7 @@ ostream& operator<<(ostream& os, const String& obj)
 
 //#define CONSTRUCTORS_CHECK
 #define OPERATOR_PLUS_CHECK
+//#define WAYS_TO_CALL_CONSTRUCTORS
 
 void main()
 {
@@ -148,4 +149,23 @@ void main()
 	str1 += str2;
 	cout << str1 << endl;
 #endif // OPERATOR_PLUS_CHECK
+
+#ifdef WAYS_TO_CALL_CONSTRUCTORS
+	String str1;		//DefaultConstructor
+	str1.print();
+	String str2(5);		//Single-argumant type 'int'
+	str2.print();
+	String str3 = "Hello";//Single-argument type 'const char*'
+	str3.print();
+	String str4();	//Это выражение НЕ создает объект, а просто объявляет функцию str4(),
+					//которая ничего не принимает, и возвращает значение типа 'String'.
+	//str4.print();
+	//Если нужно явно вызвать конструктор по умолчанию, то это можно сделать так:
+	String str5{};	//Явный вызов конструктора по умолчанию
+	str5.print();
+	String str6{ 7 };
+	str6.print();
+	String str7{ "Hello" };
+	str7.print();
+#endif // WAYS_TO_CALL_CONSTRUCTORS
 }
